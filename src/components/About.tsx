c1d0500dd9792db2df2442fd4e7439c1ca873d20
HenrikJavaYH <henrik_champion@hotmail.com>
2025-10-02 14:23:54 +0200

    Minimal text change for test purpose

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,7 +4,7 @@ export default function About() {
       <h2>Om mig</h2>
       <p>
         Hej! Jag heter Henrik Granbohm och studerar vid Stockholms Tekniska Institut till Javautvecklare.
-        Jag gillar att skapa moderna webbappar med React och TypeScript bland annat och att koda i allmänhet.
+        Jag gillar att skapa moderna webbappar med React och TypeScript bland annat och att skapa hemsidor i allmänhet.
 
         Just denna sida är byggd med främsta syfte att den ska deployas till molnet och är därför inte helt färdigställd och
         finpolerad ännu. Kolla gärna runt på sidan och kika gärna på mina projekt och vad jag kan som utvecklare.
